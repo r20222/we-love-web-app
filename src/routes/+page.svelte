563b58913d8d48fe784d52a7c4048b9c234223f9
r20222 <101579892+r20222@users.noreply.github.com>
2023-10-01 21:01:58 +0200

    Svelte parallax

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,88 @@
-<section class="parallax-container">
-    <header>
-        <div class="first-img"></div>
-        <div class="second-img"></div>
-        <div class="third-img"> <img src="/black-code-cat-large-outlined-good.svg" alt="A cat coding behind a computer."></div>
+<script>
+    let scroll
+</script>
 
-    </header>
-    <article>
-        <h1>hoi!</h1>
-        <p>Blablalblablalba</p>
-    </article>
+<svelte:window bind:scrollY={scroll} />
+
+<!-- <h1>{scroll}</h1> -->
+<header>
+    <div class="first-img" style:transform={`translate3d(0, ${scroll * -0.5}px,0)`}></div>
+    <div class="second-img" style:transform={`translate3d(0, ${scroll * -0.3}px,0)`}></div>
+    <div class="third-img"></div>
+</header>
+<!-- <main>
+<section>
+    <h1>Welcome to my We Love Web Blog!</h1>
 </section>
+</main> -->
 
+
+
+
+<!-- svelte paralax bron https://www.youtube.com/watch?v=K3CM7j9GIxk -->
 <style>
     :global(body){
         margin: 0rem;
+        height:200vh;
+        overflow-y: auto;
+        overflow-x: hidden;
     }
-/* yt bron https://www.youtube.com/watch?v=mxHoPYFsTuk */
-/* verwijderen bg: https://www.remove.bg/ & inkscape */
-    .parallax-container{
+    /* h1{
+        color:blue;
+
+    } */
+    /* .parallax-container{
+        height:100vh;
+        overflow-y: auto;
+        overflow-x: hidden;
+        perspective:10px;
+    } */
+
+    .first-img, .second-img, .third-img{
+        position: absolute;
+        height:100%;
+        width:100%;
+        object-fit: cover;
+        /* om tekst zichtbaar te maken */
+        z-index: -1;
+    }
+    .first-img{
+        height:100vh;
+        width: 100vw;
+        background: url('/sky.jpg');
+        background-position: center;
+        background-size: 100%;
+        /* transform: translateZ(-15px) scale(2.5); */
+
+    }
+    .second-img{
+        height:100vh;
+        background: url('/buildings.png');
+        background-position: center;
+        background-size: 100%;
+        top:10rem;
+        /* z-index: 100; */
+        /* transform: translateZ(-10px) scale(2); */
+    }
+
+    .third-img{
+        height:100vh;
+        background: url('/black-code-cat-large-outlined-good.svg');
+        background-position: center;
+        background-size: 100%;
+        z-index: 100;
+    }   
+    /* section{
+        width:100vw;
+        height:100vh;
+        background-color: black;
+        position: relative;
+        top: 100vh;
+        margin-top:0;
+        padding-top:0;
+    } */
+
+    /* .parallax-container{
         height:100vh;
         overflow-y: auto;
         overflow-x: hidden;
@@ -38,7 +103,7 @@
         width:100%;
         object-fit: cover;
         /* om tekst zichtbaar te maken */
-        z-index: -1;
+        /* z-index: -1;
     }
     .first-img{
         height:100vh;
@@ -55,7 +120,7 @@
         background-position: center;
         background-size: 100%;
         /* z-index: 100; */
-        transform: translateZ(-10px) scale(2);
+        /* transform: translateZ(-10px) scale(2);
     }
     .third-img{
         height:100vh;
@@ -63,15 +128,7 @@
         background-position: center;
         background-size: 100%;
         z-index: 100;
-    }
+    }   */
 
-    article{
-        height:60vh;
-        background-color: black;
-    }
-    h1{
-        margin-top:0;
-    }
-    /* bron paralax: https://blog.logrocket.com/create-parallax-scrolling-css/ */
 
 </style>
