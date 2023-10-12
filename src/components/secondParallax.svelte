f53e5f9588590985ecea0dfd415131b2f31c1e80
r20222 <101579892+r20222@users.noreply.github.com>
2023-10-12 18:18:55 +0200

    update second parallax scroll

--- src/components/secondParallax.svelte
+++ src/components/secondParallax.svelte
@@ -1,3 +1,10 @@
+<script>
+    let scroll
+ </script>
+ 
+ <svelte:window bind:scrollY={scroll} />
+ 
+
 <section class="second-parallax">
     <div class="second-parallax-first-img" style:transform={`translate3d(0, ${scroll * -0.5}px,0)`}></div>
     <div class="second-parallax-second-img" style:transform={`translate3d(0, ${scroll * -0.3}px,0)`}></div>
@@ -8,7 +15,8 @@
     .second-parallax{
         width:100vw;
         height:100vh;
-        background-color: black;
+        background-image: url('/cow.jpg');
+        background-size: 100%;
         overflow: hidden;
     }
 </style>
