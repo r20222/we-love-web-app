fef4f3002042ce84cd085bc762aaa5d6fc693ac2
r20222 <101579892+r20222@users.noreply.github.com>
2024-01-14 16:58:10 +0100

    background images on blog buttons

--- src/components/choose-blog.svelte
+++ src/components/choose-blog.svelte
@@ -2,18 +2,20 @@
     import SecondParallax from '../components/secondParallax.svelte'
 
     export let data 
+    console.log(data)
 </script>
 
 <section>
     <h2>Kies een blog:</h2>
     {#each data.dataHygraph.weLoveWebBlogs.slice(0, 3) as blog}
-            <a href="/{blog.slug}">{blog.title}</a>
+    <!-- Inline css style om de image uit cms als achtergrond te geven -->
+            <a href="/{blog.slug}" style="background-image: url({blog.image.url})">{blog.title}</a>  
     {/each}
     </section>
     <SecondParallax />
     <section>
         {#each data.dataHygraph.weLoveWebBlogs.slice(3) as blog}
-            <a href="/{blog.slug}">{blog.title}</a>
+            <a href="/{blog.slug}" style="background-image: url({blog.image.url})">{blog.title}</a>
         {/each}
     </section>
     
@@ -39,5 +41,7 @@
         padding:2rem 1rem;
         margin: 1.5rem auto;
         border: 3px solid black;
+        background-size:contain;
+        background-blend-mode: saturation;
     }
 </style>
