312c8f14a4d03777de6ee33b5806dde8300421be
r20222 <101579892+r20222@users.noreply.github.com>
2024-01-14 15:46:38 +0100

    new-design, slug page blogs

---
+++ src/components/choose-blog.svelte
@@ -0,0 +1,39 @@
+<script>
+    import SecondParallax from '../components/secondParallax.svelte'
+
+    export let data 
+</script>
+
+<section>
+    <h2>Kies een blog:</h2>
+    {#each data.dataHygraph.weLoveWebBlogs.slice(0, 3) as blog}
+            <a href="/{blog.slug}">{blog.title}</a>
+    {/each}
+    <SecondParallax />
+    {#each data.dataHygraph.weLoveWebBlogs.slice(3) as blog}
+            <a href="/{blog.slug}">{blog.title}</a>
+    {/each}
+</section>
+
+<style>
+    section{
+        position:relative;
+        background-color: var(--pink);
+        padding:1rem 5vw;
+        height: auto;
+    }
+    h2{
+        margin-top:0;
+        margin-left:1rem;
+    }
+    a{
+        display:block;
+        text-decoration: none;
+        font-size:1.5rem;
+        color: black;
+        background-color: var(--emerald);
+        padding:2rem 1rem;
+        margin: 1.5rem auto;
+        border: 3px solid black;
+    }
+</style>
---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,152 @@
+<script>
+    export let data;
+    console.log(data)
+    // console.log(data.blog.raw.children[0].type)
+</script>
+
+<section class="main">
+  <h1>{data.blog.title}</h1>
+</section>
+<!-- 
+<article>
+    <h2>{data.title}</h2>
+    <p>{data.speaker}</p>
+    <p>{data.date}</p>
+    <div> 
+        <img src="{data.image.url}" alt="{data.imageAlt}">
+    </div> -->
+
+    <!-- Blog text and lists -->
+    <!-- {#if data.blog.raw.children}     
+      {#each data.blog.raw.children as section} -->
+
+        <!-- paragraph -->
+        <!-- {#if section.type === "paragraph"}
+            <p>{section.children[0].text}</p>
+        {/if} -->
+
+        <!-- list -->
+        <!-- {#if section.type === "bulleted-list"}
+            <ul>
+                {#each section.children as listItem}
+                    <li>{listItem.children[0].children[0].text}</li>
+                {/each}
+            </ul>
+        {/if}
+
+      {/each}
+    {/if} -->
+
+  
+<!-- </article>  -->
+  <!-- desktop -->
+  <!-- <section class="desktop-image-background">
+    <section class="desktop-image">
+        <img src="{data.image.url}" alt="{data.imageAlt}" width="100">
+    </section>
+  </section> -->
+
+
+<style>
+    pre{
+        width:20rem;
+    }
+ .main{
+        width:100vw;
+        height: auto;
+        background-color: var(--pink);
+        position: relative;
+        margin-top:-.5rem;
+        padding-top:0;
+        color:black;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .main h2{
+        margin-left:5vw;
+        margin-top:0rem;
+        padding-top:1rem;
+    }
+    .main p{
+        margin-left:5vw;
+        margin-right:5vw;
+    }
+    article{
+        width:90vw;
+        height:auto;
+        background-color: var(--emerald);
+        border: 3px solid black;
+        margin-top: 2rem;
+        margin-bottom: 2rem;
+    }
+    div{
+        width:80vw;
+        height:28vh;
+        margin-left:5vw;
+        border: 3px solid black;
+    }
+    img {
+        width:100%;
+        height:100%;
+        object-fit: cover;
+    }
+    .desktop-image{
+        display:none;
+    }
+
+    @media screen and (min-width: 1000px){
+        .main{
+            width:100vw;
+            height: auto;
+            background-color: var(--pink);
+            position: relative;
+            margin-top:-.5rem;
+            padding-top:0;
+            color:black;
+            display: grid; /* Change from "flex" to "grid" */
+            grid-template-columns: 1fr 1fr; /* Two equal columns */
+            justify-content: center;
+            align-items: center;
+        }
+        article{
+            width:40vw;
+            height:auto;
+            background-color: var(--emerald);
+            border: 3px solid black;
+            margin: 2rem;
+        }
+        div{
+                display: none;
+            }
+        .desktop-image{
+            display: block;
+            width:100%;
+            display: flex;
+            justify-content: center;
+        }
+        .desktop-image-background{
+            width:100%;
+            height:100%;
+            display: flex;
+            align-items: center;
+            background-image: 
+            linear-gradient(
+            to top right in oklab, 
+            #34D8AE 0%, var(--pink) 0% 20%, #34D8AE 0% 40%, var(--pink) 0% 60%, #34D8AE 0% 80%, var(--pink) 0% 100%
+            );
+            border-left:3px solid black;
+        }
+        img{
+            width:30vw;
+            border: 3px solid black;
+        }
+
+        /* list */
+        ul{
+            padding-left:7rem;
+        }
+    }
+</style>
+
+<!-- bron om slugs te maken: https://stackoverflow.com/questions/65930303/sveltekit-how-do-i-do-slug-based-dynamic-routing -->
